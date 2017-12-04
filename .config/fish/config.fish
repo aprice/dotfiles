c506e6082d86b38c62dc16e4faf5926c82cd5533
Adrian Price <adrian.price@bluesombrero.com>
2017-12-04 11:06:57 -0500

    Fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,15 @@
+function fish_prompt
+    ~/go/bin/powerline-go -error $status -shell bare
+end
+
+set -gx GOROOT /usr/local/go
+set -gx GOPATH $HOME/go
+set -gx PATH $PATH $GOROOT/bin $HOME/.local/bin $HOME/bin $GOPATH/bin
+
+alias lla="ls -la"
+alias emptytrash="rm -rf ~/.local/share/Trash/*"
+alias duch="du -chx --max-depth=1"
+alias ts="date +%s"
+
+alias tunnel="ssh -i ~/.ssh/us-east-1_rsa ec2-user@52.91.171.110"
+
