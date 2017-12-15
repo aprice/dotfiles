a92a8b27f5d7f80b8e154b2ff27fa20092326be3
Adrian Price <adrian.price@bluesombrero.com>
2017-12-15 13:44:06 -0500

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,10 @@
 function fish_prompt
-    ~/go/bin/powerline-go -error $status -shell bare
+    ~/go/bin/powerline-go -error $status -shell bare -cwd-max-depth 4 -cwd-max-dir-size 12 -modules 'cwd,git,hg,jobs,exit,root'
 end
 
-set -gx GOROOT /usr/local/go
-set -gx GOPATH $HOME/go
-set -gx PATH $PATH $GOROOT/bin $HOME/.local/bin $HOME/bin $GOPATH/bin
+set -x GOROOT /usr/local/go
+set -x GOPATH $HOME/go
+set -x PATH $PATH $GOROOT/bin $HOME/.local/bin $HOME/bin $GOPATH/bin
 
 alias lla="ls -la"
 alias emptytrash="rm -rf ~/.local/share/Trash/*"
